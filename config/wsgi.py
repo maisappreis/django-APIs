ee97364a7f831d47fbcb7d2a82f5a396bb3656ab
Maisa Preis <maisapierinipreis@gmail.com>
2024-08-13 11:37:39 -0300

    fix: Trying to get deploy working on Vercel.

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,14 +8,15 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-from pathlib import Path
-from dotenv import load_dotenv
+# from pathlib import Path
+# from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-env_path = BASE_DIR / '.env'
-load_dotenv(dotenv_path=env_path)
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# env_path = BASE_DIR / '.env'
+# load_dotenv(dotenv_path=env_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+app = application
