1075c0dc5cd60dff8778c1289e0918a13c7d8ff5
Maisa Preis <maisapierinipreis@gmail.com>
2024-08-13 19:45:49 -0300

    fix: Trying to use SQLite on Vercel to test.

--- config/settings.py
+++ config/settings.py
@@ -99,7 +99,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        # 'NAME': BASE_DIR / "db.sqlite3",
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
