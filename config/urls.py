f83184cc4776670a8735aa384ee84e8496a9ea66
Maisa Preis <maisapierinipreis@gmail.com>
2024-08-16 19:03:20 -0300

    feat: Ensuring that only authenticated users have access to certain views.

--- config/urls.py
+++ config/urls.py
@@ -13,14 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path, include
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/', include('upfit_gym.urls')),
-# ]
-
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -30,4 +22,5 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/', include('upfit_gym.urls')),
+    path('api/accounts/', include('accounts.urls')),
 ]
--- upfit_gym/views.py
+++ upfit_gym/views.py
@@ -1,60 +1,62 @@
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework import status
 from rest_framework import generics
 from .models import *
 from .serializers import *
 
+# Real views used by authenticated users.
+
+
+from rest_framework.permissions import IsAuthenticated
 
 class CustomerListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
 
 class CustomerCreateView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
 
 class CustomerUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
 
-# class CustomerListView(APIView):
-
-#     def get(self, request, *args, **kwargs):
-#         customers = Customer.objects.all()
-#         serializer = CustomerSerializer(customers, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
-
 class ExpenseListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
 
 
 class ExpenseCreateView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
 
 
 class ExpenseUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
 
 
 class RevenueListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Revenue.objects.all()
     serializer_class = RevenueSerializer
 
 
 class RevenueCreateView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Revenue.objects.all()
     serializer_class = RevenueSerializer
 
 
 class RevenueUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Revenue.objects.all()
     serializer_class = RevenueSerializer
 
