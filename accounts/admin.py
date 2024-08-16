bb71fb9dfa9cfbd33337b5921d3b73db38fd8e06
Maisa Preis <maisapierinipreis@gmail.com>
2024-08-16 18:57:05 -0300

    feat: Setting admin settings.

---
+++ accounts/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from rest_framework.authtoken.models import Token
+
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('key', 'user', 'created')
+
--- upfit_gym/admin.py
+++ upfit_gym/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import Customer, Expense, Revenue
+from .models_test import CustomerTest, ExpenseTest, RevenueTest
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'frequency', 'start', 'plan', 'value', 'status')
+    search_fields = ('name', 'plan')
+    list_filter = ('status', 'frequency')
+    ordering = ('name',)
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'month', 'due_date', 'value', 'paid')
+    search_fields = ('name',)
+    list_filter = ('paid', 'year', 'month')
+    ordering = ('year', 'month')
+
+
+@admin.register(Revenue)
+class RevenueAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'year', 'month', 'payment_day', 'value', 'paid')
+    search_fields = ('customer__name',)
+    list_filter = ('paid', 'year', 'month', 'customer')
+    ordering = ('customer', 'year', 'month')
+
+
+@admin.register(CustomerTest)
+class CustomerTestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'frequency', 'start', 'plan', 'value', 'status')
+    search_fields = ('name', 'plan')
+    list_filter = ('status', 'frequency')
+    ordering = ('name',)
+
+
+@admin.register(ExpenseTest)
+class ExpenseTestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'month', 'due_date', 'value', 'paid')
+    search_fields = ('name',)
+    list_filter = ('paid', 'year', 'month')
+    ordering = ('year', 'month')
+
+
+@admin.register(RevenueTest)
+class RevenueTestAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'year', 'month', 'payment_day', 'value', 'paid')
+    search_fields = ('customer__name',)
+    list_filter = ('paid', 'year', 'month', 'customer')
+    ordering = ('customer', 'year', 'month')
