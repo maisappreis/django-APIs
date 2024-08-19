d142134462d7b579886d18042bc90446947a1803
Maisa Preis <maisapierinipreis@gmail.com>
2024-08-18 22:57:04 -0300

    feat: Adding logout api.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from accounts.views import *
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     # path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
