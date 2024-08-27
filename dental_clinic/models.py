fcd4559d33cc114edb7a3c88c62c33ee31c33a68
Maisa Preis <maisapierinipreis@gmail.com>
2024-08-27 19:49:16 -0300

    feat: Creating the real models used by authenticated users.

--- dental_clinic/models.py
+++ dental_clinic/models.py
@@ -1,5 +1,43 @@
 from django.db import models
 
+# Real models used by authenticated users.
+
+
+class Revenue(models.Model):
+    date = models.DateField(null=False, blank=False)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    cpf = models.CharField(max_length=14, null=False, blank=True)
+    nf = models.BooleanField(default=False, null=False, blank=False)
+    procedure = models.CharField(max_length=255, null=False, blank=False)
+    payment = models.CharField(max_length=255, null=False, blank=False)
+    installments = models.IntegerField(null=False, blank=False)
+    value = models.FloatField(null=False, blank=False)
+    notes = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        unique_together = ['date', 'name', 'procedure', 'value']
+
+
+class Expense(models.Model):
+    year = models.IntegerField(null=False, blank=False)
+    month = models.CharField(max_length=50, null=False, blank=False)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    installments = models.CharField(max_length=50, null=False, blank=True)
+    date = models.DateField(null=False, blank=False)
+    value = models.FloatField(null=False, blank=False)
+    is_paid = models.BooleanField(default=False, null=False, blank=False)
+    notes = models.TextField(null=True, blank=True)
+   
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        unique_together = ['name', 'year', 'month', 'value']
+
+
 # Test models used by unauthenticated users test application, like a portfolio.
 
 
