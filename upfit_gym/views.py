3c839839a54984f9f61d046a853da5f42a582d73
Maisa Preis <maisapierinipreis@gmail.com>
2024-08-30 17:38:18 -0300

    fix: Fixing for deploy.

--- upfit_gym/views.py
+++ upfit_gym/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics
 from .models import *
 from .serializers import *
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from dental_clinic.utils import createInstallments
 
 # Real views used by authenticated users.
 
@@ -52,6 +54,23 @@ class ExpenseCreateView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        installments = data.get('installments', "")
+
+        if installments == "":
+            return super().create(request, *args, **kwargs)
+        
+        serializer, created_objects = createInstallments(
+            serializer_class=self.get_serializer_class(),
+            perform_create=self.perform_create,
+            installments=installments,
+            data=data
+        )
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(created_objects, status=201, headers=headers)
 
 
 class ExpenseUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
