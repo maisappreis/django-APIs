9e1332bc768f6f517b9e91b5908328c00fd2f3c7
Maisa Preis <maisapierinipreis@gmail.com>
2024-12-02 19:40:32 -0300

    feat: Creating scripts to download database data.

---
+++ dental_clinic/management/commands/export_clinic_data.py
@@ -0,0 +1,27 @@
+import os
+import json
+from datetime import date, datetime
+from django.core.management.base import BaseCommand
+from dental_clinic.models import Revenue, Expense, MonthClosing
+
+
+def custom_serializer(obj):
+    if isinstance(obj, (date, datetime)):
+        return obj.isoformat()
+    raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
+
+class Command(BaseCommand):
+    help = 'Exporta dados do banco de dados para um arquivo JSON'
+
+    def handle(self, *args, **kwargs):
+        data = list(Expense.objects.all().values())
+        
+        directory = 'dental_clinic/database'
+        json_file_path = os.path.join(directory, 'expense2024.json')
+        
+        os.makedirs(directory, exist_ok=True)
+        
+        with open(json_file_path, 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4, default=custom_serializer)
+        
+        self.stdout.write(self.style.SUCCESS(f'Dados exportados para {json_file_path}'))
