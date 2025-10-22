13ca3128d3d7b50a580992e27316bf75305542a9
Maisa Preis <maisapierinipreis@gmail.com>
2025-10-22 11:29:10 -0300

    fix: Tentando resolver problemas de ALLOWED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -101,7 +101,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
-    "https://maisappreis.github.io"
+    "https://maisappreis.github.io",
+    "https://maisappreis.github.io/",
+    "https://maisappreis.github.io/axis/"
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
