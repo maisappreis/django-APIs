404b2f442e71563fb6c19981cca7703d06705d43
Maisa Preis <maisapierinipreis@gmail.com>
2026-03-10 20:05:59 -0300

    feat: Return the user to the frontend.

---
+++ accounts/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data["user"] = {
+            "id": self.user.id,
+            "username": self.user.username,
+            "first_name": self.user.first_name,
+            "last_name": self.user.last_name,
+            "email": self.user.email,
+        }
+
+        return data
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from accounts.views import *
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from accounts.views import CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
